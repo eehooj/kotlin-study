48ff59259e4b181bb9901458f7d52e4149e86d01
zuri.jeong <zuri.jeong@onda.me>
2023-11-06 18:50:27 +0900

    권한 관리

---
+++ kotlin-security/src/main/kotlin/com/example/kotlinsecurity/common/authority/JwtTokenProvider.kt
@@ -0,0 +1,87 @@
+package com.example.kotlinsecurity.common.authority
+
+import io.jsonwebtoken.*
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+
+const val EXPIRATION_MILLISECONDS: Long = 100 * 60 * 30
+
+@Component
+class JwtTokenProvider {
+
+    @Value("\${jwt.secret}")
+    lateinit var secretKey: String
+
+    private val key by lazy { Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)) }
+
+    /**
+     * Token 생성
+     */
+    fun createToken(authentication: Authentication): TokenInfo {
+        val authorities: String = authentication
+            .authorities
+            .joinToString(",", transform = GrantedAuthority::getAuthority)
+        val now = Date()
+        val accessExpiration = Date(now.time + EXPIRATION_MILLISECONDS)
+        val accessToken = Jwts
+            .builder()
+            .claim("auth", authorities)
+            .subject(authentication.name)
+            .issuedAt(now)
+            .expiration(accessExpiration)
+            .signWith(key)
+            .compact()
+
+        return TokenInfo("Bearer", accessToken)
+    }
+
+    /**
+     * Token 정보 추출
+     */
+    fun getAuthentication(token: String): Authentication {
+        val claims: Claims = getClaims(token)
+        val auth = claims["auth"] ?: throw RuntimeException("잘못된 토큰입니다.")
+        val authorities: Collection<GrantedAuthority> = (auth as String)
+            .split(",")
+            .map { SimpleGrantedAuthority(it) }
+        val principal: UserDetails = User(claims.subject, "", authorities)
+
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    /**
+     * Token 검증
+     */
+    fun validateToken(token: String): Boolean {
+        try {
+            getClaims(token)
+
+            return true
+        } catch (e: Exception) {
+            when (e) {
+                is SecurityException -> {}
+                is MalformedJwtException -> {}
+                is ExpiredJwtException -> {}
+                is UnsupportedJwtException -> {}
+                is IllegalArgumentException -> {}
+                else -> {}
+            }
+
+            println(e.message)
+        }
+
+        return false
+    }
+
+    private fun getClaims(token: String): Claims =
+        Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload
+}
---
+++ kotlin-security/src/main/kotlin/com/example/kotlinsecurity/common/authority/SecurityConfig.kt
@@ -0,0 +1,41 @@
+package com.example.kotlinsecurity.common.authority
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/member/signup").anonymous()
+                    .anyRequest().permitAll()
+            }
+            .addFilterBefore(
+                JwtAuthenticFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter::class.java
+            // 뒤에 필터 보다 앞의 필터를 먼저 실행. JwtAuthenticFilter가 통과되면 UsernamePasswordAuthenticationFilter 실행X
+            )
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+}
